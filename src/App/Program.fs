e632907e18bb2f607df969b3fcd56dd9a3f8e65a
iitd.parvez@gmail.com <parvez@pfakhruddin.fios-router.home>
2018-07-27 21:12:29 -0400

    dotnet core skeleton

---
+++ src/App/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Library.Say.hello argv.[0]
+    0 // return an integer exit code
